afbc183734febf8e61c90ba39729215463810c2e
sjdy521 <sjdy521@163.com>
2017-11-30 15:18:55 +0800

    版本更新

---
+++ perl/site/lib/Mojo/Webqq/AccountLogin.pm
@@ -0,0 +1,12 @@
+package Mojo::Webqq::AccountLogin;
+sub account_login {
+    my($account,$password,$service) = @_;
+}   
+sub account_login_qzone{
+}
+sub account_login_email{
+}
+sub account_login_qun{
+
+}
+1;
